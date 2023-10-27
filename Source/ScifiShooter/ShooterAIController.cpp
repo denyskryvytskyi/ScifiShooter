23341fcdcd662a38799a878c680885c8963c2b2d
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2023-10-27 23:50:07 +0300

    Added simplpe behavior to the Enemy Behavior Tre

--- Source/ScifiShooter/ShooterAIController.cpp
+++ Source/ScifiShooter/ShooterAIController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ShooterAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Kismet\GameplayStatics.h"
 
 void AShooterAIController::BeginPlay()
@@ -9,10 +10,24 @@ void AShooterAIController::BeginPlay()
 
     if (AIBehavior) {
         RunBehaviorTree(AIBehavior);
+
+        if (APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0)) {
+            GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+            GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+        }
     }
 }
 
 void AShooterAIController::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
+
+    if (APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0)) {
+        if (LineOfSightTo(PlayerPawn)) {
+            GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+            GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+        } else {
+            GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+        }
+    }
 }
