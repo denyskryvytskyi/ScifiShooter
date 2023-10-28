4a42e4064bdbc0d761b569dbf27c70687ce04a0a
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2023-10-28 13:24:45 +0300

    Added custom BTTask to enable Enemy AI return to start location

---
+++ Source/ScifiShooter/BTTask_ClearBlackboardValue.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "CoreMinimal.h"
+
+#include "BTTask_ClearBlackboardValue.generated.h"
+/**
+ *
+ */
+UCLASS()
+class SCIFISHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase {
+    GENERATED_BODY()
+
+public:
+    UBTTask_ClearBlackboardValue();
+
+    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
+};
